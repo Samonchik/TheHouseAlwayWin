5ce6cbdad0da46a714457a4dd48c1ea10c9a1717
Siamen Kadtsyn <kadcino@gmail.com>
2022-10-29 15:06:01 +0300

    Добавьте файлы проекта.

---
+++ TheHouseAlwayWin/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TheHouseAlwayWin
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.54;
+            Guy player = new Guy() { Name = "the player", Cash = 100 };
+            Console.WriteLine($"Welcom to the casino. The odds are {odds}");
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+                Console.Write("How much do you want to bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount) * 2;
+                    if (pot > 0)
+                    {
+
+                        if (random.NextDouble() > odds)
+                        {
+                            player.ReciveCash(pot);
+                            Console.WriteLine($"\tYou win {pot}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\tBad Luck. You lose.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter correct number");
+                    }
+
+                }
+            }
+
+            Console.WriteLine("The house always win.");
+        }
+    }
+}
